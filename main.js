f433c8de9d906e08acfdc5f941ee48516f7beabd
Luan Oak <99412966+luancarvalho7@users.noreply.github.com>
2024-09-09 00:30:40 +0000

    test

--- main.js
+++ main.js
@@ -18,12 +18,14 @@ let xquizData = [];
 let quizRanking = [];
 
 async function getData() {
+
     try {
-        const response = await fetch(`https://webhook.workez.online/webhook/findQuiz?url=${videoUrl}`);
+        const response = await fetch(`https://n8nwebhook.iatom.site/webhook/findQuiz?url=${videoUrl}`);
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
         const data = await response.json();
+        console.log('oi')
 
         // Log the data to understand its structure
         console.log("Fetched data:", data);
